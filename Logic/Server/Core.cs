6e92347b46fefedbaa26f8f89f4ec2aff6edbb0c
Ted Liou <mail@tedliou.com>
2022-04-29 22:43:16 +0800

    Create basic script

---
+++ Logic/Server/Core.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Core
+    {
+        public static ILoggerFactory? LoggerFactory { get; set; }
+
+        private readonly ILogger<Core> _logger;
+
+        public Core()
+        {
+            LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+            {
+                builder.AddFilter("Microsoft", LogLevel.Warning)
+                       .AddFilter("System", LogLevel.Warning)
+                       .AddConsole();
+            });
+            _logger = LoggerFactory.CreateLogger<Core>();
+        }
+    }
+}
--- Logic/Server/Program.cs
+++ Logic/Server/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Server;
+
+new Core();
+
+Console.ReadKey();
